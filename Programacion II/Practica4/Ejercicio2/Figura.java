ea514ddb073c9c96bba9a0c1ff27a956b4e29177
ivan-uz <70560225+ivan-uz@users.noreply.github.com>
2025-09-29 00:13:05 -0400

    Create Figura.java

---
+++ Programacion II/Practica4/Ejercicio2/Figura.java
@@ -0,0 +1,20 @@
+package coloreado;
+
+public abstract class Figura {
+	private String color;
+	public Figura(String color) {
+		this.color=color;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public abstract double Area();
+	public abstract double Perimetro();
+	@Override
+	public String toString() {
+		return "Figura [color=" + color + "]";
+	}
+}
