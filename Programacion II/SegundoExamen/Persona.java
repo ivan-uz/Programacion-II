0921b15b61c66c17daa5d787fc635a84074cd631
ivan-uz <70560225+ivan-uz@users.noreply.github.com>
2025-11-17 09:30:02 -0400

    Create Persona.java

---
+++ Programacion II/SegundoExamen/Persona.java
@@ -0,0 +1,30 @@
+package sss;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	private float pesoPersona;
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+
+	public float getPesoPersona() {
+		return pesoPersona;
+	}
+
+	public Persona(String nombre, int edad, float pesoPersona) {
+
+		this.nombre = nombre;
+		this.edad = edad;
+		this.pesoPersona = pesoPersona;
+	}
+	
+	
+	
+	
+
+}
