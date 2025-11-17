4de17f93bcdee22fb01d8e4819c63e3e02aca09f
ivan-uz <70560225+ivan-uz@users.noreply.github.com>
2025-11-17 09:44:20 -0400

    Create MiTeleferico class with initial setup

---
+++ Programacion II/SegundoExamen/MiTeleferico.java
@@ -0,0 +1,24 @@
+package sss;
+
+public class MiTeleferico {
+	private Linea[] lineas;
+	private float cantidadIngresos;
+	
+	public MiTeleferico() {
+		this.lineas = new Linea("Amarrilo",p,c,can);
+		this.cantidadIngresos = 0;
+	}
+	
+	public void agregarPersonFila(Persona p,String linea){
+		for(int i;i<)
+	}
+	
+	public void agregarCabina(){
+		
+	}
+	
+	
+	
+	
+
+}
